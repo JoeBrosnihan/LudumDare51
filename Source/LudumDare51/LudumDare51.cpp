5ed982953f993376f9f06b70faa1e7c9d022ad38
Matthew McCloud <m.mcclouddev@gmail.com>
2022-10-01 13:06:14 -0500

    - redo IT ALL

--- Source/LudumDare51/LudumDare51.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "LudumDare51.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LudumDare51, "LudumDare51" );
- 
